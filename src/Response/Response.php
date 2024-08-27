33fa05851a8ac1ebd19d20e46c4f4dccee6ebaf0
Joachim Løvgaard <joachim@loevgaard.dk>
2024-08-27 13:58:27 +0200

    Allow guzzlehttp/psr7 v2

--- src/Response/Response.php
+++ src/Response/Response.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Setono\GLS\Webservice\Response;
 
-use function GuzzleHttp\Psr7\parse_response;
+use GuzzleHttp\Psr7\Message;
 use GuzzleHttp\Psr7\Response as PsrResponse;
 use stdClass;
 
@@ -16,7 +16,7 @@ final class Response
 
     public function __construct(string $headers, string $body, ?stdClass $result)
     {
-        $this->response = parse_response($headers . "\r\n" . $body);
+        $this->response = Message::parseResponse($headers . "\r\n" . $body);
         $this->result = $result;
     }
 
